66601d09cedf19ecb38fe81bd0f30e11b0e2b2b9
aonitcmtc <patchara_aon_1@hotmail.com>
2024-02-22 12:09:43 +0700

    update contact&footer

---
+++ js/footer.js
@@ -0,0 +1,100 @@
+var en_name = '7fc5598a904c98df151e75af932fc4f6'; //Like FLOOR StampConcrete
+// my_footer
+var myFooter = $("#my_footer");
+
+var div_code = `<div class="footer-content position-relative">
+                  <div class="container">
+                  <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                      <div class="footer-info">
+                        <h3>Like Floor StampConcrete</h3>
+                          <p>ถนน เชียงใหม่-ลำปาง, ช้างเผือก, <br>หมู่ที่ 5, เชียงใหม่ 50300, ประเทศไทย</p>
+                          <p>Chiang Mai, Thailand 50300</p>
+                          <br>
+                          <strong>Phone:</strong> 084 610 6657<br>
+                          <strong>Email:</strong> LikeFloorStampConcrete@gmail.com<br>
+                        <div class="social-links d-flex mt-3">
+                          <a href="javascript:void(0)" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+                          <a href="https://www.facebook.com/likefloorstampconcrete" target="_blank" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                          <a href="javascript:void(0)" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+                          <a href="https://lin.ee/nknUyhZ" target="_blank" class="d-flex align-items-center justify-content-center"><i class="bi bi-line"></i></a>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- End footer info column-->
+                    <div class="col-lg-2 col-md-3 footer-links">
+                      <h4>Useful Links</h4>
+                      <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Terms of service</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                      </ul>
+                    </div>
+                    <!-- End footer links column-->
+                    <div class="col-lg-2 col-md-3 footer-links">
+                      <h4>Our Services</h4>
+                      <ul>
+                        <li><a href="#">Web Design</a></li>
+                        <li><a href="#">Web Development</a></li>
+                        <li><a href="#">Product Management</a></li>
+                        <li><a href="#">Marketing</a></li>
+                        <li><a href="#">Graphic Design</a></li>
+                      </ul>
+                    </div>
+                    <!-- End footer links column-->
+                    <div class="col-lg-2 col-md-3 footer-links">
+                      <h4>Hic solutasetp</h4>
+                      <ul>
+                        <li><a href="#">Molestiae accusamus iure</a></li>
+                        <li><a href="#">Excepturi dignissimos</a></li>
+                        <li><a href="#">Suscipit distinctio</a></li>
+                        <li><a href="#">Dilecta</a></li>
+                        <li><a href="#">Sit quas consectetur</a></li>
+                      </ul>
+                    </div>
+                    <!-- End footer links column-->
+                    <div class="col-lg-2 col-md-3 footer-links">
+                      <h4>Nobis illum</h4>
+                      <ul>
+                        <li><a href="#">Ipsam</a></li>
+                        <li><a href="#">Laudantium dolorum</a></li>
+                        <li><a href="#">Dinera</a></li>
+                        <li><a href="#">Trodelas</a></li>
+                        <li><a href="#">Flexo</a></li>
+                      </ul>
+                    </div>
+                    <!-- End footer links column-->
+                    </div>
+                  </div>
+                </div>
+                <div class="footer-legal text-center position-relative">
+                  <div class="container">
+                    <div class="copyright">
+                      &copy; Copyright By <strong><span>aonitcmtc</span></strong>.
+                    </div>
+                    <div class="credits">
+                      <!-- All the links in the footer should remain intact. -->
+                      <!-- You can delete the links only if you purchased the pro version. -->
+                      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ -->
+                      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                    </div>
+                  </div>
+                </div>`;
+myFooter.append(div_code);
+
+// $('.stock-btn').on('click', function() {
+//   $('.stock-btn').removeClass('active');
+//   id = $(this).attr('id')
+//   $('#'+id).addClass('active');
+//   name_label = $(this).data('type');
+//   $('#title_name_label').text(name_label)
+// });
+
+// function init () {
+//   $('#stock').addClass('active');
+// }
+
+// init();
